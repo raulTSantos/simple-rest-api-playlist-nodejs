3e11a302c5c0d3deb9d475f84ba21045166b8590
raulTSantos <confox24.s@gmail.com>
2023-01-23 22:20:29 -0500

    clean of comments

--- src/components/playlist/controller.ts
+++ src/components/playlist/controller.ts
@@ -2,32 +2,6 @@ import type { Request, Response } from "express";
 
 import prisma from "../../datasource";
 
-
-/* export const createPlaylist = async (req: Request, res: Response): Promise<void> =>{
-    try {
-        const {name,user_id,songs} = req.body;
-       
-        const  playlist = await prisma.playlist.create({
-            data: {
-                name: name,
-                users: {connect : {id:user_id}},
-                songs: {connect: Array.isArray(songs) ? songs.map((x:number) =>({id:x})) : [{id:songs}]},
-
-            }
-        });
-        res.status(201).json({
-            ok:true,
-            data: playlist,
-        });
-    } catch (error) {
-        console.log(error);
-        
-        res.status(500).json({
-            ok:false,
-            message: error
-        });
-    }
-}; */
 export const findAllPlaylists = async (req: Request, res: Response): Promise<void> =>{
     try {
         const {user_id}= req.body;
@@ -39,7 +13,6 @@ export const findAllPlaylists = async (req: Request, res: Response): Promise<voi
                 include: {songs: true}
             },
         );
-        console.log(playlist);
         
         res.status(200).json({
             ok: true,
@@ -50,28 +23,7 @@ export const findAllPlaylists = async (req: Request, res: Response): Promise<voi
     }
     
 }
-export const addSongToPlaylists = async (req: Request, res:Response): Promise<void> =>{
-    try {
-        const {playlist_id,song_id,name} = req.body;
-        const  playlist = await prisma.playlist.update({
-            where:  {id:playlist_id},
-            data:{
-                name:name,
-                songs:{connect :Array.isArray(song_id) ? song_id.map((x:number) =>({id:x})):[{id:song_id}]}
-            } 
-        });
-        console.log(playlist);
-        
-        res.status(200).json({
-            ok: true,
-            data: playlist,
-        });
-    } catch (error) {
-        console.log(error);
-        
-        res.status(500).json({ ok: false, message: error });
-    }
-};
+
 export const createOrUpdatePlaylist = async (req: Request, res:Response): Promise<void> =>{
     try {
         const {playlist_id,song_id,name,user_id} = req.body;
@@ -97,8 +49,6 @@ export const createOrUpdatePlaylist = async (req: Request, res:Response): Promis
             data: playlist,
         });
     } catch (error) {
-        console.log(error);
-        
         res.status(500).json({ ok: false, message: error });
     }
 }
--- src/components/playlist/index.ts
+++ src/components/playlist/index.ts
@@ -5,8 +5,7 @@ import * as Controller from "./controller";
 const playlist_router = Router();
 
 playlist_router.route("/").get(Controller.findAllPlaylists);
-//playlist_router.route("/").post(Controller.createPlaylist);
 playlist_router.route("/").post(Controller.createOrUpdatePlaylist);
-playlist_router.route("/").put(Controller.addSongToPlaylists);
+
 
 export default playlist_router;
